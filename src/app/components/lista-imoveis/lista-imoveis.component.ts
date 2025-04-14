c5a504f8f538827dc09dc5b4f79b03d59e472937
Alessandro <alessandro.1982silva@gmail.com>
2025-04-14 08:54:33 -0300

    exibir lista de imoveis atualizado

--- src/app/components/lista-imoveis/lista-imoveis.component.ts
+++ src/app/components/lista-imoveis/lista-imoveis.component.ts
@@ -12,26 +12,71 @@ import { ImovelService } from '../../imovel.service';
 })
 export class ListaImoveisComponent {
 
+  carregando = true;
   imoveis: any[] = [];
+  imoveisPaginados: any[] = [];
+  paginaAtual = 1;
+  itensPorPagina = 4;
+  totalPaginas = 1;
 
   constructor(private imovelService: ImovelService) {}
 
   ngOnInit(): void {
+    this.carregando = true;
+
     this.imovelService.listarImoveis().subscribe({
       next: (res) => {
-        if (res) {
-          this.imoveis = Object.values(res).map((imovel: any) => ({
-            imagem: imovel.imagem,
-            endereco: imovel.endereco
+        if (res && Object.keys(res).length > 0) {
+          const imoveisBrutos = Object.values(res).map((imovel: any) => ({
+            imagem: imovel.imagem || '',
+            endereco: imovel.endereco || '',
+            valor: imovel.valor || '',
+            tipo: imovel.tipo || ''
           }));
+
+          // Filtra apenas os imóveis completos
+          this.imoveis = imoveisBrutos.filter(imovel =>
+            imovel.imagem && imovel.endereco && imovel.valor && imovel.tipo
+          );
+
+          this.totalPaginas = Math.ceil(this.imoveis.length / this.itensPorPagina);
+          this.atualizarPagina();
         } else {
           this.imoveis = [];
+          this.imoveisPaginados = [];
         }
+
+        this.carregando = false;
       },
       error: (err) => {
         console.error('Erro ao carregar imóveis:', err);
+        this.imoveis = [];
+        this.imoveisPaginados = [];
+        this.carregando = false;
       }
     });
   }
+
+  atualizarPagina(): void {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    this.imoveisPaginados = this.imoveis.slice(inicio, fim);
+  }
+
+  proximaPagina(): void {
+    if (this.paginaAtual < this.totalPaginas) {
+      this.paginaAtual++;
+      this.atualizarPagina();
+    }
+  }
+
+  paginaAnterior(): void {
+    if (this.paginaAtual > 1) {
+      this.paginaAtual--;
+      this.atualizarPagina();
+    }
+  }
+
+  
   
 }

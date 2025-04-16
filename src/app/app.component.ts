536a84615bc0f8a47f9509321d754c56e81d0323
Alessandro <alessandro.1982silva@gmail.com>
2025-04-15 22:20:19 -0300

    Editar imóvel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonImovelComponent } from './button-imovel/button-imovel.component';
-import { ButtonAcaoComponent } from './button-acao/button-acao.component';
 import { ListaImoveisComponent } from './components/lista-imoveis/lista-imoveis.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonImovelComponent, ButtonAcaoComponent, ListaImoveisComponent],
+  imports: [RouterOutlet, ButtonImovelComponent,  ListaImoveisComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/detalhes-imovel/detalhes-imovel.component.ts
+++ src/app/detalhes-imovel/detalhes-imovel.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { ImovelService } from '../imovel.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detalhes-imovel',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './detalhes-imovel.component.html',
   styleUrl: './detalhes-imovel.component.css'
 })
 export class DetalhesImovelComponent {
   imovel: any;
-
+  editando = false; 
   constructor(
     private router: Router,
     private location: Location,
@@ -26,14 +27,33 @@ export class DetalhesImovelComponent {
   voltar(): void {
     this.location.back();
   }
+
   editar() {
-    this.router.navigate(['/editar-imovel'], { state: { imovel: this.imovel } });
+    this.editando = true;
   }
-  
+
+  salvar() {
+    if (!this.imovel?.idFirebase) {
+      alert('ID do imóvel não encontrado!');
+      return;
+    }
+
+    this.imovelService.atualizarImovel(this.imovel.idFirebase, this.imovel).subscribe({
+      next: () => {
+        alert('Imóvel atualizado com sucesso!');
+        this.editando = false;
+      },
+      error: (err) => {
+        console.error('Erro ao atualizar imóvel:', err);
+        alert('Erro ao salvar as alterações.');
+      }
+    });
+  }
+
   excluir() {
     const confirmado = window.confirm('Deseja mesmo excluir este imóvel?');
     if (!confirmado) return;
-  
+
     this.imovelService.deletarImovel(this.imovel.idFirebase).subscribe({
       next: () => {
         alert('Imóvel excluído com sucesso!');
@@ -47,4 +67,19 @@ export class DetalhesImovelComponent {
     });
   }
   
+  novaImagem: File | null = null;
+
+selecionarNovaImagem(event: Event) {
+  const input = event.target as HTMLInputElement;
+  if (input?.files && input.files.length > 0) {
+    this.novaImagem = input.files[0];
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imovel.imagem = reader.result as string;
+    };
+    reader.readAsDataURL(this.novaImagem);
+  }
+}
+
 }
